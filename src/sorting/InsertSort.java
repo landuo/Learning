d371d6c93727ddbdcf3e0ea4cb0a1cbc55e1536d
accidia.huang <accidia.huang@accentrix.com>
2020-08-31 16:58:52 +0800

    Change the exchange method

--- src/sorting/InsertSort.java
+++ src/sorting/InsertSort.java
@@ -18,10 +18,10 @@ public class InsertSort {
             // 从当前需要排序的数的前一个逆序遍历至数组第一个元素
             while (j >= 0 && arr[j] > num) {  // 一直向前对比，直到 arr[j] 比 num 小为止
                 // 进入这里说明 arr[i] 比 num ，需要把 num 放到 arr[i] 前面 （即二者交换位置）
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                // 交换完后继续与前面的元素对比，直至遍历到数组第一个元素
+                arr[j] = arr[j] ^ arr[j + 1];
+                arr[j + 1] = arr[j] ^ arr[j + 1];
+                arr[j] = arr[j] ^ arr[j + 1];
+                // 交换完后继续与前面的元素对比
                 j--;
             }
             System.out.println("第" + i + "次排序：" + Arrays.toString(arr));
